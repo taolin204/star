72094bee2e12b23bd6326b551d91546a5a9bc350
TAO LIN <t4ol1n@gmail.com>
2013-03-06 12:12:40 +0800

    update interviewee view/show

--- app/models/interviewee.rb
+++ app/models/interviewee.rb
@@ -1,4 +1,6 @@
 class Interviewee < ActiveRecord::Base
-  attr_accessible :address, :first_name, :last_name, :nationality, :postal_code
+  attr_accessible :address, :first_name, :last_name, :nationality, :postal_code, :gender,
+  					:race, :mobile, :telephone, :religion, :email, :dob, :specialization, :prefer_industry,
+  					:country, :state, :willing_travel, :willing_relocate, :job_nature, :language, :remark
   has_many :schedules, dependent: :destroy
 end
